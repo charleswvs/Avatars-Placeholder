7baa035679b2941c4078738d5836fd477200ba3b
Charles Willian <cwillian@luxurypresence.com>
2025-02-25 20:04:22 -0300

    feat: Improve server binding to support all network interfaces

--- back-end/app/index.ts
+++ back-end/app/index.ts
@@ -1,33 +1,33 @@
-const express = require('express')
-const cors = require('cors');
-const bodyParser = require('body-parser')
-import router from "./routes"
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+import router from "./routes";
 
-const app = express()
+const app = express();
 
-export default class Application{
-    constructor(){
-        this.configServer();
-        this.config();
-        this.setRoutes();
-    }
+export default class Application {
+  constructor() {
+    this.configServer();
+    this.config();
+    this.setRoutes();
+  }
 
-    configServer(){
-        app.listen(process.env.PORT, (err: object)=>{
-            if(err) console.log(err);
-            console.log(`server run on port ${process.env.PORT} ....`)
-            if(process.env.MODE == 'dev') console.log("Development mode is enabled :)")
-        })
-    }
+  configServer() {
+    app.listen(process.env.PORT, "::", (err: object) => {
+      if (err) console.log(err);
+      console.log(`server run on port ${process.env.PORT} ....`);
+      if (process.env.MODE == "dev")
+        console.log("Development mode is enabled :)");
+    });
+  }
 
-    config(){
-        app.use(bodyParser.json())
-        app.use(bodyParser.urlencoded({extended: true}))
-        app.use(cors())
-    }
+  config() {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
+  }
 
-    setRoutes(){
-        app.use(router)
-    }
-    
+  setRoutes() {
+    app.use(router);
+  }
 }
